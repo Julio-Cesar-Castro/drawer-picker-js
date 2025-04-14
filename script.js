f31cbd91ca2716e9e047e85622be85e81f177bec
Julio Castro <juliocastro.contato@gmail.com>
2025-04-14 15:45:01 -0300

    Building CSS and JS

--- script.js
+++ script.js
@@ -3,15 +3,25 @@ const quantity = document.querySelector("#quantity")
 const firstChoose = document.querySelector("#firstChoose")
 const secondChoose = document.querySelector("#secondChoose")
 const form = document.querySelector("form")
+const contentDraw = document.querySelector(".container-draw")
 const checkbox = document.querySelector("input[type='checkbox']")
 
-
+// Gloval Variables
 let arrayNumbers = []
 
+
+// Functions
+/**
+ * 
+ * @param {Number} max  Maximum number that the user wants to keep in range
+ * @param {Number} min  Minimum number that the user wants to keep in range
+ * @returns 
+ */
 function  getRandomNumber(max, min){
   return Math.floor(Math.random() * ((max + 1) - min) + min)
 }
 
+// Events
 form.addEventListener('submit', (e) => {
   e.preventDefault()
   
@@ -31,6 +41,16 @@ form.addEventListener('submit', (e) => {
     
     arrayNumbers.push(number)
   }
+
+  
+  arrayNumbers.forEach(drawer => {
+    let span = document.createElement('span')
+
+    span.classList.add('draw')
+    span.innerText = drawer
+
+    contentDraw.append(span)
+  })
 
   console.log(arrayNumbers)
 })
